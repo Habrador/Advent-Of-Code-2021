b64f48b50abfa661a6ad71c8335edce28bfff4a8
habrador <erik.nordeus@gmail.com>
2021-12-09 13:38:57 +0100

    Added part 1 of day 9

--- Assets/Code/Day_09.cs
+++ Assets/Code/Day_09.cs
@@ -21,7 +21,7 @@ public class Day_09 : MonoBehaviour
         //Debug.Log(allRowsString.Length);
         //Debug.Log(allRowsString[0].Length);
 
-        int[,] map = new int[allRowsString.Length, allRowsString[0].Length];
+        int[,] heightMap = new int[allRowsString.Length, allRowsString[0].Length];
 
         for (int row = 0; row < allRowsString.Length; row++)
         {
@@ -29,13 +29,84 @@ public class Day_09 : MonoBehaviour
 
             for (int col = 0; col < thisRow.Length; col++)
             {
-                map[row, col] = thisRow[col] - '0';
+                heightMap[row, col] = thisRow[col] - '0';
             }
         }
 
-        Debug.Log(map[0, 0]); //7
-        Debug.Log(map[0, 99]); //5
-        Debug.Log(map[99, 0]); //6
-        Debug.Log(map[99, 99]); //9
+        //Debug.Log(heightMap[0, 0]); //7
+        //Debug.Log(heightMap[0, 99]); //5
+        //Debug.Log(heightMap[99, 0]); //6
+        //Debug.Log(heightMap[99, 99]); //9
+
+        //Each number in the height map represents a height (9 is highest, 0 is lowest)
+
+        //Find the low points (the height in the cell is lower than the 4 surrounding cells)
+        //The risk level of a low point is height + 1
+        //Find all risk levels and sum them up
+
+        int totalRiskLevel = 0;
+
+        int mapSize = heightMap.GetLength(0);
+
+        for (int x = 0; x < mapSize; x++)
+        {
+            for (int y = 0; y < mapSize; y++)
+            {
+                int height = heightMap[x, y];
+
+                bool areSurroundingCellsHigher = true;
+
+                //Go thorugh the 4 surrounding cells and see if they are higher
+                if (IsWithinMap(x - 1, y + 0, mapSize))
+                {
+                    if (heightMap[x - 1, y + 0] <= height)
+                    {
+                        areSurroundingCellsHigher = false;
+                    }
+                }
+                if (IsWithinMap(x + 0, y + 1, mapSize))
+                {
+                    if (heightMap[x + 0, y + 1] <= height)
+                    {
+                        areSurroundingCellsHigher = false;
+                    }
+                }
+                if (IsWithinMap(x + 1, y + 0, mapSize))
+                {
+                    if (heightMap[x + 1, y + 0] <= height)
+                    {
+                        areSurroundingCellsHigher = false;
+                    }
+                }
+                if (IsWithinMap(x + 0, y - 1, mapSize))
+                {
+                    if (heightMap[x + 0, y - 1] <= height)
+                    {
+                        areSurroundingCellsHigher = false;
+                    }
+                }
+
+                if (areSurroundingCellsHigher)
+                {
+                    totalRiskLevel += height + 1;
+                }
+            }
+        }
+
+
+        //Should be 535
+        Debug.Log($"Total risk level: {totalRiskLevel}");
+    }
+
+
+    private bool IsWithinMap(int x, int y, int mapSize)
+    {
+        //Make sure this cell is within the map
+        if (x < 0 || x >= mapSize || y < 0 || y >= mapSize)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
